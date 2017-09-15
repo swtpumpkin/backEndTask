53844e7083af238fd6aaa2f88c0fd4b29c40d1d1
JeongwonKim <swtpumpkin90@gmail.com>
2017-09-15 13:41:36 +0900

    Create migrations folder & files.

--- wpsn-knex/knexfile.js
+++ wpsn-knex/knexfile.js
@@ -1,44 +1,15 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'mysql',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.DATABASE_HOST,
+      user:  process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    debug: true
   }
-
 };
--- wpsn-knex/src/knex.js
+++ wpsn-knex/src/knex.js
@@ -1,13 +1,3 @@
-require('dotenv').config()
+const config = require('../knexfile')
 
-module.exports = require('knex')({
-  client: 'mysql',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    // 실제 서비스에서는 root 계정을 사용하지 않는 것이 좋습니다.
-    user:  process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
-  },
-  debug: true
-})
+module.exports = require('knex')(config.development)
