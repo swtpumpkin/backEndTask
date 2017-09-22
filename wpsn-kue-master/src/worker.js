3f64667230d847a073a1c22279695c77c9fcdcfc
JeongwonKim <swtpumpkin90@gmail.com>
2017-09-22 11:17:10 +0900

    원본 이미지 다운로드 후 썸네일 생성 후 썸네일 URL수정

--- wpsn-kue-master/src/worker.js
+++ wpsn-kue-master/src/worker.js
@@ -11,8 +11,27 @@ const queue = kue.createQueue()
 queue.process('thumbnail', (job, done) => {
   const {id} = job.data
   // 이미지 항목 정보를 데이터베이스에서 가져온 후
-  // 원본 이미지 다운로드
-  // 썸네일 생성
-  // 썸네일 업로드
-  // 이미지 항목의 썸네일 URL 수정
+  query.getImageEntryById(id)
+    .then(imageEntry => {
+      // 원본 이미지 다운로드
+      axios.get(imageEntry.original_url, {
+        responseType: 'arrayBuffer'
+      }).then(res => {
+        // 썸네일 생성
+        return sharp(res.data)
+          .resize(200, 200)
+          .crop(sharp.gravity.center)
+          .toBuffer()
+      }).then(buffer => {
+        // 썸네일 업로드(어디에 업로드 되어있는지 주소를 갖고온다.)
+        return image.uploadImageFile(buffer)
+      }).then(location => {
+        // 이미지 항목의 썸네일 URL 수정
+        return query.updateThumbnailUrlByid(id, location)
+      }).then(() => {
+        done()
+      }).catch(err => {
+        done(err)
+      })
+    })
 })
