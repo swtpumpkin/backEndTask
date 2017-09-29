6bf75a4dd12b7f278b6e0287d8236e8396dbe824
JeongwonKim <swtpumpkin90@gmail.com>
2017-09-29 17:50:32 +0900

    Code indentation.

--- kue-playground/sharp.js
+++ kue-playground/sharp.js
@@ -4,8 +4,8 @@ const sharp = require('sharp')
 const buffer = fs.readFileSync('node.png')
 
 sharp(buffer)
-.resize(200, 200)
-.crop(sharp.gravity.center)
-.toFile('output.png', (err, info) => {
-  console.log(info)
+  .resize(200, 200)
+  .crop(sharp.gravity.center)
+  .toFile('output.png', (err, info) => {
+    console.log(info)
 })
